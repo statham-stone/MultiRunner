63ddcd54c2e23b74acfa4af86e631d47e5dd36ad
Statham <statham.stone@gmail.com>
2018-03-26 11:04:28 +0800

    add github url

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 setup(
     name = "MultiRunner",
-    version = "4.0",
-    description= "Add error info output, change parameter type for running user-defined main function",
+    version = "4.1",
+    description= "Add github url",
     long_description= "This is a package for multi-process running, dedicated to my girlfriend 10L",
-    url='https://www.github.com/statham-stone/',
+    url='https://github.com/Statham-stone/MultiRunner',
     author = "Statham",
     author_email = "statham.stone@gmail.com",
     packages=[""]

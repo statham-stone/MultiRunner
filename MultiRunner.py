5d0637c8526e952cf66dd9f49492aae5bfe9955d
cclauss <cclauss@bluewin.ch>
2018-03-28 12:30:03 +0200

    print() is a function in Python 3

--- MultiRunner.py
+++ MultiRunner.py
@@ -4,12 +4,13 @@
 # This package is dedicated to my girlfriend 10L.
 # ALL RIGHTS RESERVED
 
-import pickle
 import itertools
 import os
-import random
 import pickle
+import random
 import traceback
+
+
 class MultiRunner():
     def __init__(self,ini_dir="./ini/",result_dir="./results/",max_error_times=5,if_log=True):
         if ini_dir[-1]!="/":
@@ -30,21 +31,21 @@ class MultiRunner():
             if len(os.listdir(self.ini_dir))>0:
                 if reserve_old_ini:
                     if self.if_log:
-                        print "Ini files already generated."
+                        print("Ini files already generated.")
                     return
                 else:
                     if self.if_log:
-                        print "There are old ini files to be deleted.\nPlease delete the old ini files manually and try again."
+                        print("There are old ini files to be deleted.\nPlease delete the old ini files manually and try again.")
                     return
         count=1
         for i in itertools.product(*args_list):
             if show_ini:
-                print i
+                print(i)
             pickle.dump(i,open(self.ini_dir+str(count)+"_to_run","w"))
             count+=1
         count-=1
         if self.if_log:
-            print "All "+str(count)+" ini files generated."
+            print("All "+str(count)+" ini files generated.")
         
     def _find_files(self,_type):
         all_files=filter(lambda x:x[0]!=".",os.listdir(self.ini_dir))
@@ -89,20 +90,20 @@ class MultiRunner():
             num=np.array(map(lambda y:float(y[0][:-3])/float(y[1][:-3]),filter(lambda j:len(j)==2,map(lambda x:filter(lambda k:k.find("MiB")>-1,x.split(" ")),all_lines)))).argmin()
             torch.cuda.set_device(num)
             if self.if_log:
-                print "Use gpu: "+str(num)
+                print("Use gpu: "+str(num))
 
         while(error_time<self.max_error_times): 
             find_a_para,para_path=self._return_a_para()
             if find_a_para==[]:
                 if self.if_log:
-                    print "All finished, no new task to run."
+                    print("All finished, no new task to run.")
                 break
             else:
                 try:
                     result=main_function(*find_a_para)
                     if os.path.isfile(para_path[:-8]+"_finished"):
                         if self.if_log:
-                            print "This job is already done by others"                        
+                            print("This job is already done by others")
                     else:# maybe running by others at the same time
                         pickle.dump(result,open(self.result_dir+para_path[len(self.ini_dir):-8],"w"))
                         try:
@@ -116,17 +117,17 @@ class MultiRunner():
                 except Exception:
                     error_time+=1
                     if self.if_log:
-                        print "Error info:"
+                        print("Error info:")
                         traceback.print_exc()
-                        print "Error! Total error times: "+str(error_time)
+                        print("Error! Total error times: "+str(error_time))
                     try:
                         os.rename(para_path,para_path[:-8]+"_to_run")
                         if self.if_log:
-                            print "Rename"+para_path+" to "+para_path[:-8]+"_to_run"
+                            print("Rename"+para_path+" to "+para_path[:-8]+"_to_run")
                     except Exception: #changed to finished or torun by others
                         if self.if_log:
-                            print "This job is already done or reverted by others"
+                            print("This job is already done or reverted by others")
                     
             if error_time>=self.max_error_times:
                 if self.if_log:
-                    print "Too many error times, break."
+                    print("Too many error times, break.")

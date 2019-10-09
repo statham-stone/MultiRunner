2b0a97dcda9d629743b0c9184e687369f7aed0a5
Statham <statham.stone@gmail.com>
2019-10-09 20:20:49 +0800

    emm

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name = "MultiRunner",
+    version = "4.6",
+    description= "This is a python package for multi-process running.",
+    long_description= "This is a package for multi-process running, dedicated to my girlfriend 10L",
+    url='https://github.com/Statham-stone/MultiRunner',
+    author = "Statham",
+    author_email = "statham.stone@gmail.com",
+    packages=[""]
+)
